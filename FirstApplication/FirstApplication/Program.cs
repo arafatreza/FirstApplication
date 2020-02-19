b2e3df298e081ee0edb429b1970b8aa058a402ad
arafatreza <arafat.reza1997@gmail.com>
2020-02-19 17:14:56 +0600

    FirstApplication added v-0.1

--- FirstApplication/FirstApplication/Program.cs
+++ FirstApplication/FirstApplication/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 
 namespace FirstApplication
 {
@@ -6,22 +7,31 @@ namespace FirstApplication
     {
         static void Main(string[] args)
         {
-            const string conn = "Server = ; Database = ; User Id = ; Password = ; ";
+            const string conn = "Server = DESKTOP-28DHLU6; Database = test; User Id = arafat; Password = 123; ";
 
-            using (var connection = new SqlConnect(conn))
+            using (var connection = new SqlConnection(conn))
             {
                 try
                 {
                     connection.Open();
-                    var sql = "sql command";
+                    var sql = "Insert into Login ([Name],Email,Contact,[Address],Username,[Password],[Type]) Values ('A','a@gmail.com','123','mirpur','arafat','1234','user');";
                     using (var command = new SqlCommand(sql , connection))
                     {
-                        command.ExecuteNonQuery();
+                        
+                        int a = command.ExecuteNonQuery();
+                        if(a==1)
+                        {
+                            Console.WriteLine("Data Inserted");
+                        }
+                       
+                       
+
                     }
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine(e.Message);
+                    //Console.WriteLine(e.Message);
+                    Console.WriteLine("Data Not Inserted");
                 }
                 finally
                 {

5af2f9580d520f5798c068723ded87e4e0857d17
arafatreza <arafat.reza1997@gmail.com>
2020-02-19 13:42:01 +0600

    FirstApplication added

---
+++ FirstApplication/FirstApplication/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FirstApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string conn = "Server = ; Database = ; User Id = ; Password = ; ";
+
+            using (var connection = new SqlConnect(conn))
+            {
+                try
+                {
+                    connection.Open();
+                    var sql = "sql command";
+                    using (var command = new SqlCommand(sql , connection))
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    connection?.Close();
+                }
+            }
+        }
+    }
+}
